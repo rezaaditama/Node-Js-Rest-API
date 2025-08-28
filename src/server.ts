ae980383db6b39abd19f990637eba4945b75e90b
Reza Aditama <aditamareza03@gmail.com>
2025-08-28 17:14:23 +0700

    initialize project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use('/health', (req: Request, res: Response) => {
+  res.status(400).send({ status: 200 });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
