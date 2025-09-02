ccc8bbee2d4df949ac9d5f282b09d702c5801150
Reza Aditama <aditamareza03@gmail.com>
2025-09-02 20:23:22 +0700

    chore: setup Husky pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,33 +35,3 @@ export default defineConfig([
     },
   },
 ]);
-
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import tseslint from 'typescript-eslint';
-// import { defineConfig } from 'eslint/config';
-
-// export default defineConfig([
-//   {
-//     ignores: ['**/dist/*', '**/node_modules/*', '**/public/*', 'eslint.config.mjs', '**/tsconfig.json'],
-//   },
-//   {
-//     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-//     plugins: { '@typescript-eslint': tseslint.plugin, js },
-//     extends: ['js/recommended', ...tseslint.configs.recommended],
-//     languageOptions: {
-//       parser: tseslint.parser,
-//       parserOptions: { project: ['./tsconfig.json'] },
-//       sourceType: 'module',
-//       globals: globals.node,
-//     },
-//     rules: {
-//       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-//       '@typescript-eslint/strict-boolean-expressions': 'off',
-//       'no-return-assign': 'off',
-//       '@typescript-eslint/restrict-template-expressions': 'off',
-//       '@typescript-eslint/explicit-function-return-type': 'off',
-//       'array-callback-return': 'off',
-//     },
-//   },
-// ]);
